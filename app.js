4d2e5c61a69598d8bf666b7305a2b592c7231ca1
MardonovD <mardonovdoston69@gmail.com>
2022-04-09 14:32:47 +0500

    form create

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 "use strict";
 
-//1.. https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.json 
-
-// 2.. https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ben-muhiuddinkhan.json --> quran yozuvi...
+const container = document.querySelector(".sura");
 
 async function func() {
-  const link = await fetch(
-    ``
-  );
-  const linkJson = await link.json();
-  console.log(linkJson);
+  const uzgaruvchi = await fetch(`https://api.quran.sutanlab.id/surah`);
+  const suralar = await uzgaruvchi.json();
+  const sarr = suralar.data;
+
+  console.log(sarr);
+
+  sarr.forEach((element) => {
+    let html = ``;
+
+    html = `
+     <div class="cart">
+     <div class="circl">${element.number}</div>
+    <div class="box">
+      <p>${element.name.short}</p>
+      <p>${element.revelation.arab}</p>
+    </div>
+    </div>  
+    `;
+    container.insertAdjacentHTML("afterend", html);
+  });
 }
 
 func();
