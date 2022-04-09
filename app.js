a3ec0bf5b455d6f340330494902db7839ad288dc
MardonovD <mardonovdoston69@gmail.com>
2022-04-09 15:50:26 +0500

    new

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const container = document.querySelector(".sura");
+let cart;
+const play = document.querySelector(".play");
 
 async function func() {
   const uzgaruvchi = await fetch(`https://api.quran.sutanlab.id/surah`);
@@ -13,16 +15,26 @@ async function func() {
     let html = ``;
 
     html = `
-     <div class="cart">
+     <div class="cart" id="${element.number}">
      <div class="circl">${element.number}</div>
     <div class="box">
-      <p>${element.name.short}</p>
-      <p>${element.revelation.arab}</p>
+      <p>${element.name.transliteration.en}</p>
+      <p>${element.name.long}</p>
     </div>
     </div>  
+
     `;
     container.insertAdjacentHTML("afterend", html);
   });
+  cart = document.querySelector(".cart");
 }
 
 func();
+
+async function oo() {
+  const urll = await fetch(`https://api.quran.sutanlab.id/surah/1`);
+  const javob = await urll.json();
+  console.log(javob.data.verses[0].audio.primary);
+}
+
+oo();
